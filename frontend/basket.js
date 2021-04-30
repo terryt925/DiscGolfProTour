ec26a28594fb9681cc608c842e8b10054a4ae611
terryt925 <thindterry@yahoo.com>
2021-04-30 09:52:54 -0700

    Change routing

--- frontend/basket.js
+++ frontend/basket.js
@@ -10,7 +10,7 @@ class Basket {
 
   placeBasket(ctx) {
     let basket = new Image();
-    basket.src = '../public/basket.png';
+    basket.src = './public/basket.png';
     if (this.hole === 1) {
       basket.onload = function(){
       ctx.drawImage(basket, 1000, 260, 60, 100);
